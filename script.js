67a31e38b576faaaef7b628d88326ec419210f40
Troy Bohrer <tbohrer96@gmail.com>
2021-06-10 21:53:27 -0400

    adding updated javascript

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var options = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()+={[}]<>/?;:"
 
 // Write password to the #password input
+function askforOptions () {
+  
+
+  if (passwordLength === "" || passwordLength < 8 || passwordLength > 120){
+    alert("Invaild Entry") 
+      return;
+  } else {(isNaN(passwordLength)) 
+      alert("Invalid Entry");
+
+  return;
+    }
+
+}
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -11,9 +26,22 @@ function writePassword() {
 }
 
 function generatePassword() {
-  var passwordLength = prompt("How many characters do you want?")
-  console.log(passwordLength)
+
+  var passwordLength = prompt("How many characters do you want?");
+  console.log(passwordLength);
+
+ var length = passwordLength;
+ var result = ''
+
+  for (var i  = 0; i < options.length; i++) {
+    result += options.charAt(Math.floor(Math.random() * length));
+    return result;
+  }
+
 }
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
